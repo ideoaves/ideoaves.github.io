056b10fc6e70548dd91ad25f750b6c65d3261659
ideoaves <105560914+ideoaves@users.noreply.github.com>
2022-12-09 17:50:45 +0900

    Update cursor.js

--- cursor.js
+++ cursor.js
@@ -1,4 +1,4 @@
-var colour=""blue""; // in addition to "random" can be set to any valid colour eg "#f0f" or "red"
+var colour="blue"; // in addition to "random" can be set to any valid colour eg "#f0f" or "red"
 var sparkles=50;
 
 /****************************
@@ -7,10 +7,10 @@ var sparkles=50;
 *  http://www.mf2fm.com/rv  *
 * DON'T EDIT BELOW THIS BOX *
 ****************************/
-var x=ox=400;
-var y=oy=300;
-var swide=800;
-var shigh=600;
+var x=ox=1000;
+var y=oy=1000;
+var swide=1000;
+var shigh=1000;
 var sleft=sdown=0;
 var tiny=new Array();
 var star=new Array();
@@ -56,7 +56,7 @@ function sparkle() {
     for (c=0; c<sparkles; c++) if (!starv[c]) {
       star[c].style.left=(starx[c]=x)+"px";
       star[c].style.top=(stary[c]=y+1)+"px";
-      star[c].style.clip="rect(0px, 5px, 5px, 0px)";
+      star[c].style.clip="rect(0px, 10px, 10px, 0px)";
       star[c].childNodes[0].style.backgroundColor=star[c].childNodes[1].style.backgroundColor=(colour=="random")?newColour():colour;
       star[c].style.visibility="visible";
       starv[c]=50;
@@ -73,7 +73,7 @@ function sparkle() {
 function update_star(i) {
   if (--starv[i]==25) star[i].style.clip="rect(1px, 4px, 4px, 1px)";
   if (starv[i]) {
-    stary[i]+=1+Math.random()*3;
+    stary[i]-=1+Math.random()*6;
     starx[i]+=(i%5-2)/5;
     if (stary[i]<shigh+sdown) {
       star[i].style.top=stary[i]+"px";
@@ -103,7 +103,7 @@ function update_tiny(i) {
     tiny[i].style.height="1px";
   }
   if (tinyv[i]) {
-    tinyy[i]+=1+Math.random()*3;
+    tinyy[i]-=1+Math.random()*6;
     tinyx[i]+=(i%5-2)/5;
     if (tinyy[i]<shigh+sdown) {
       tiny[i].style.top=tinyy[i]+"px";
